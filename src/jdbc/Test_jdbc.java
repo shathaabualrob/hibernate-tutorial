a2b2223dd225d8ffc72e34aa616cf781d936b448
Shatha AR <shatharub5@gmail.com>
2023-05-15 15:09:26 +0300

    Initial commit

---
+++ src/jdbc/Test_jdbc.java
@@ -0,0 +1,29 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Test_jdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcURL = "jdbc:mysql://localhost:3306/hb_student_tracker?"
+				+ "allowPublicKeyRetrieval=true&useSSL=false"
+				+ "&serverTimezone=UTC";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcURL);
+			
+			@SuppressWarnings("unused")
+			Connection myconn = DriverManager.getConnection(jdbcURL, user, pass);
+			
+			System.out.println("Connection Successful!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
